8179719c0f1f04bcc58e798e0ab4887b2bbc43a7
velagala Durga Sai Gangireddy <153359680+Sai-Reddy-dev@users.noreply.github.com>
2026-03-18 10:45:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Binary Search/binary-search.py
@@ -0,0 +1,19 @@
+class Solution:
+    def binarysearch(self, arr, x):
+        # Code Here
+        low = 0
+        high = len(arr) - 1
+        while low <= high:
+    
+            mid = low + (high - low) // 2
+    
+            if arr[mid] == x and arr[mid-1] != k: 
+                return mid
+    
+            elif arr[mid] < x:
+                low = mid + 1
+    
+            else:
+                high = mid - 1
+
+        return -1
