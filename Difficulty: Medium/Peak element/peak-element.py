250344525e0a32b3bdbad290970be1835981cf68
velagala Durga Sai Gangireddy <153359680+Sai-Reddy-dev@users.noreply.github.com>
2026-03-25 20:43:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Peak element/peak-element.py
@@ -0,0 +1,26 @@
+class Solution:   
+    def peakElement(self, nums):
+        # Code here
+        n = len(nums)
+
+        if n == 1:
+            return 0
+        if nums[0] > nums[1]:
+            return 0
+        if nums[n-1] > nums[n-2]:
+            return n-1
+        
+        low , high = 1,n-2
+
+        while(low <= high):
+            mid = (low + high) // 2
+
+            if nums[mid-1] < nums[mid] and nums[mid+1] < nums[mid]:
+                return mid
+            
+            if nums[mid-1]  < nums[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+        
